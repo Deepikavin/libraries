980fe6f719fbecc0e6c9747f8ceeebbcfa5ff427
Deepikavin <116055801+Deepikavin@users.noreply.github.com>
2024-05-21 18:45:36 +0100

    Update math.groovy

--- vars/math.groovy
+++ vars/math.groovy
@@ -4,14 +4,14 @@ def sum(a,b)
 }
 def sub(a,b)
 {
-  echo "the sum of ${a} and ${b} is ${a-b}"
+  echo "the sub of ${a} and ${b} is ${a-b}"
 }
 def product(a,b)
 {
-  echo "the sum of ${a} and ${b} is ${a*b}"
+  echo "the product of ${a} and ${b} is ${a*b}"
 }
 def rem(a,b)
 {
-  echo "the sum of ${a} and ${b} is ${a/b}"
+  echo "the rem of ${a} and ${b} is ${a/b}"
 }
 
